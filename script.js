fe939fdd5eb6cab21adffad4065844805359a392
eliotbosman <114088053+eliotbosman@users.noreply.github.com>
2022-09-21 16:49:27 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+var grid = document.getElementsByClassName("grid")[0];
+var info = document.getElementsByClassName("info")[0];
+
+function infopg() {
+    grid.style.display = "none";
+    info.style.display = "grid";
+    info.style.animation = "fadein linear 1.2s";
+}
+
+function homepg() {
+    info.style.display = "none";
+    grid.style.display = "grid";
+    grid.style.animation = "fadein linear 1.2s";
+}
+
+function skroll() {
+    window.scroll({
+        top: 0,
+        left: 0,
+        behavior: 'smooth'
+      }); 
+}
